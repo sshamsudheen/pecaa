9139709b385dd9dc3fac53133be5100892a0c941
Venkat <venkat.muvva@gmail.com>
2012-04-11 23:34:20 +0530

    Futher fine tuning of Map in Miscs tab

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,10 @@ class Address < ActiveRecord::Base
   belongs_to :user
   belongs_to :site
   belongs_to :created_by, :class_name => "User", :foreign_key => "created_by" 
+  
+  def address_string
+    "#{address_one} #{address_two} #{city}, #{state} - #{zipcode} #{country}"
+    
+  end
+  
 end
