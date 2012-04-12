45ba19d4dff38861ac4133cae8fcb91294dca0e9
Venkat <venkat.muvva@gmail.com>
2012-04-12 23:07:52 +0530

    Misc tab loading the map according to site address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ActiveRecord::Base
   belongs_to :created_by, :class_name => "User", :foreign_key => "created_by" 
   
   def address_string
-    "#{address_one} #{address_two} #{city}, #{state} - #{zipcode} #{country}"
+    "#{address_one} #{address_two}, #{city}, #{state}  #{zipcode}, #{country}"
     
   end
   
