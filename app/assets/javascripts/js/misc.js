b5c5138ae8051b8ff7ec5761f3e4e28d1486359f
Venkat <venkat.muvva@gmail.com>
2012-04-18 23:25:14 +0530

    Added missing js file

---
+++ app/assets/javascripts/js/misc.js
@@ -0,0 +1,24 @@
+
+	
+	
+	 function loadMap() {
+	 	var address = $("#mapAddress").val();
+	    var uri = "zoom=10&size=400x400&markers=color:blue|" + encodeURIComponent(address) + "&sensor=false";
+	    var mapURL = "http://maps.googleapis.com/maps/api/staticmap?" + uri; //encodeURIComponent(uri); 
+	    $("#map-canvas-image").attr('src', mapURL);
+	}
+	
+	  function initMisc() {
+		$('#mapObj').css("left", "50px");
+	    $('#mapObj').css("top", "30px");
+	    $('#mapAddr').css("right", "50px");
+	    $('#mapAddr').css("top", "30px");
+	    loadMap();
+		$("#reloadMap").click(function(){
+			loadMap();
+			return false;
+		});	
+		
+	}
+	
+	
