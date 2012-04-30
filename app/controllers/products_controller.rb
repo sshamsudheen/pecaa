e1aaac554e8cc0b16f361edaa437836d011ae795
Venkat <venkat.muvva@gmail.com>
2012-04-30 10:02:55 +0530

    Implementation of Create/Edit product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,16 @@
 class ProductsController < ApplicationController
   def index
   end
+  
+  
+  # GET /products/new
+  
+  def new
+    # temporary fix 
+    @product = Product.new
+    respond_to do |format|
+      format.html { render :layout => false}# new.html.erb
+    end
+  end
 
 end
