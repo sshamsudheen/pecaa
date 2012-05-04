86754dd8ec6d411be2e8c364cf9b71bd90bac994
rajeshv <rajesh@ras.beamtele.net>
2012-05-05 01:40:10 +0530

    [FIX] The Integration of the html templates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,63 +12,42 @@ class ProductsController < ApplicationController
   
   def new
     @product = Product.new
-    @site = Site.find(params[:site_id])
-     respond_to do |format|
-       format.html { render :layout => false}# new.html.erb
-     end
+    render :layout => false
   end
   
   
   # POST /products
   def create
     @product = Product.new(params[:product])
-    @site = Site.find(params[:site_id])
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to site_products_url(@site) }
-      else
-        format.html { render action: "new" }
-      end
+    if @product.save
+      redirect_to site_products_url(@site)
+    else
+      render :action => "new"
     end
   end
-  
-  
   
   # PUT /products/1
   def update
     @product = Product.find(params[:id])
-    respond_to do |format|
-      if @product.update_attributes(params[:product])
-        format.html { redirect_to products_url, notice: 'Misc was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @misc.errors, status: :unprocessable_entity }
-      end
+    if @product.update_attributes(params[:product])
+      redirect_to products_url
+    else
+      render :action => "edit"
     end
   end
   
   private
+  
   def ensure_site_id
-    site_id = params[:site_id]
-    @site = Site.find(site_id) if site_id
-    if (site_id.blank? || @site.blank?) 
-     respond_to do |format|
+    unless @site = Site.find_by_id(params[:site_id])
       #format.html { render template: 'errors/error_404', layout: 'layouts/application', status: 404 }
-      format.all { render nothing: true, status: 404 }
-      end
+      render :nothing => true, :status=> 404
     end  
   end
   
-  
   def ensure_product_id
-    product_id = params[:product_id]
-    @product = Product.find(product_id) if product_id
-    if product_id.blank? || @product.blank? 
-      respond_to do |format|
-        #format.html { render template: 'errors/error_404', layout: 'layouts/application', status: 404 }
-        format.all { render nothing: true, status: 404 }
-      end
+    unless @product = Product.find_by_id(params[:product_id])
+      render :nothing => true, :status=> 404
     end  
   end
 
