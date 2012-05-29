33d44cdb26d9a9b17aa859e52a279202978197bb
unknown <Administrator@honey-478c5b988.(none)>
2012-05-29 23:18:03 +0530

    [fix] css and js changes

---
+++ app/assets/javascripts/checkScroll.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$(".ConLibra li:even").addClass("alt");
+})
+
+function validateScroll(tblId, tblHeight){
+	var $tbl = $('#'+tblId), tH = tblHeight;
+	if($tbl.length) {
+		if($tbl.find('.ConLibra').height() >= tH) {
+			$tbl.find('.scrollbar').show();
+			$tbl.find('.overview').css('position', 'absolute');
+			$tbl.find('.viewport').css('height', '400px');
+			$tbl.find('ul.ConLibra').css('border-right', '0');
+			$tbl.tinyscrollbar();
+		} else {
+			$tbl.find('.overview').css('position', 'relative');
+			$tbl.find('.scrollbar').hide();
+		}
+	}
+}
