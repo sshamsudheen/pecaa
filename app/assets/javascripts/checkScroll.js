af0f712349c1d6d17203fdaeeb0550cb6b68eea4
unknown <Administrator@honey-478c5b988.(none)>
2012-06-01 00:57:55 +0530

    [fix] css changes

--- app/assets/javascripts/checkScroll.js
+++ app/assets/javascripts/checkScroll.js
@@ -8,7 +8,7 @@ function validateScroll(tblId, tblHeight){
 		if($tbl.find('.ConLibra').height() >= tH) {
 			$tbl.find('.scrollbar').show();
 			$tbl.find('.overview').css('position', 'absolute');
-			$tbl.find('.viewport').css('height', '400px');
+			$tbl.find('.viewport').height(tH);
 			$tbl.find('ul.ConLibra').css('border-right', '0');
 			$tbl.tinyscrollbar();
 		} else {
