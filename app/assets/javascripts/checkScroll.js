217648ea60d48805800cdaa735cdcc3c451a748f
unknown <Administrator@honey-478c5b988.(none)>
2012-06-07 23:57:20 +0530

    [FIX]UI issues

--- app/assets/javascripts/checkScroll.js
+++ app/assets/javascripts/checkScroll.js
@@ -11,9 +11,11 @@ function validateScroll(tblId, tblHeight){
 			$tbl.find('.viewport').height(tH);
 			$tbl.find('ul.ConLibra').css('border-right', '0');
 			$tbl.tinyscrollbar();
+			$('#shadow').show();
 		} else {
 			$tbl.find('.overview').css('position', 'relative');
 			$tbl.find('.scrollbar').hide();
+			$('#shadow').hide();
 		}
 	}
 }
