78e6d4a21e623e44fb2291248ae42b69d69e31af
Mitesh Jain <mjain@crri.co.in>
2012-07-16 22:25:00 +0530

    fix the controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -24,11 +24,11 @@ class RegistrationsController < Devise::RegistrationsController
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        redirect_to :new
+        respond_with resource, :location => redirect_location(resource_name, resource)
       else
         set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
         expire_session_data_after_sign_in!
-        redirect_to :new
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
       end
     else
       clean_up_passwords(resource)
