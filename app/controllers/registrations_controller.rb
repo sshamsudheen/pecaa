ecb2bc1ac75ffa225b5c08eb321bf60d107acaeb
Mitesh Jain <mjain@crri.co.in>
2012-07-26 22:42:52 +0530

    added the registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -25,11 +25,11 @@ class RegistrationsController < Devise::RegistrationsController
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        respond_with resource, :location => redirect_location(resource_name, resource)
+        redirect_to new_site_registration_path(@site)
       else
         set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(resource) if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        redirect_to new_site_registration_path(@site)
       end
     else
       clean_up_passwords(resource)
