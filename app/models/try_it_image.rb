3cf77d6c0d87ad42bc177e1af3b546df8bb5743f
rajeshv <rajesh@ras.beamtele.net>
2012-07-29 01:11:53 +0530

    [MISC]

--- app/models/try_it_image.rb
+++ app/models/try_it_image.rb
@@ -7,4 +7,8 @@ class TryItImage < ActiveRecord::Base
 validates_attachment_presence :upload
 validates_attachment_size :upload, :less_than=>10.megabyte
 validates_attachment_content_type :upload, :content_type=>['image/jpeg', 'image/png', 'image/gif']
+
+def file_name
+  upload_file_name.split('.').first
+end
 end
