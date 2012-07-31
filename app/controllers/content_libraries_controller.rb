e6c165b8686865afbcfeb336d3a192de39dab0ca
shamsudheen <sshamsudheen@gmail.com>
2012-08-01 01:16:01 +0530

    1. After giving wrong login details, the error displayed is wrong. It should say 'Invalid username or password'. Currently it says 'Invalid email or password'. 2. http://rails.brightstores.com/user/password - In the above page, at the bottom section, if a username is given and the username does not exist in the system, an error message should be shown. 3. Search in content_liabraries - implemented
    Worked on the above 3

--- app/controllers/content_libraries_controller.rb
+++ app/controllers/content_libraries_controller.rb
@@ -6,6 +6,7 @@ class ContentLibrariesController < ApplicationController
   # GET /content_libraries.json
   def index
     session[:site_page_id] =nil
+	@users = User.all()
     @content_libraries = ContentLibrary.order("created_at desc")
 
     respond_to do |format|
@@ -89,13 +90,33 @@ class ContentLibrariesController < ApplicationController
     @text = Text.new()
     @misc = Misc.new()
     @site = Site.find(params[:site_id]) rescue nil
-    @site_page = SitePage.find(params[:site_page_id])
-    session[:site_page_id] = @site_page.id
-    @address = Address.find_by_site_id(params[:site_id])
-    @content_libraries = ContentLibrary.search(params[:search]).page(params[:page]).per(12)
+    #@site_page = SitePage.find(params[:site_page_id])
+    #session[:site_page_id] = @site_page.id
+    #@address = Address.find_by_site_id(params[:site_id])
+    #@content_libraries = ContentLibrary.search(params[:search]).page(params[:page]).per(12)
+	#@content_libraries = ContentLibrary.search(params[:search])
+    #respond_to do |format|
+     # format.html { render :action => "search",:layout=>"site" }
+      #format.json { head :ok }
+    #end
+	@users = User.all()
+	session[:site_page_id] =nil
+    #@content_libraries = ContentLibrary.search(params[:search])
+	@name = params[:name];
+	@type = params[:source_type]; 
+	@added_by = params[:added_by]; 
+	query_text = "name LIKE '%#{@name}%'"	
+	if @type!=''
+		query_text = query_text + " and source_type =  '#{@type}'"
+	end
+	if @added_by!=''
+		query_text = query_text + " and added_by =  '#{@added_by}'"
+	end
+	#Rails.logger.debug(query_text)
+	@content_libraries = ContentLibrary.where(query_text)
     respond_to do |format|
-      format.html { render :action => "search",:layout=>"site" }
-      format.json { head :ok }
+      format.html # index.html.erb	 
+      format.json { render :json => @content_libraries }
     end
   end
   
