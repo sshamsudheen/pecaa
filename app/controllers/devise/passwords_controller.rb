c074eb5dfd8913037310f117d1d054a22968cde7
Manikandan K <manikandan.nitt@gmail.com>
2012-08-01 22:15:07 +0530

    User Forget password changes error message added

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -11,14 +11,15 @@ class Devise::PasswordsController < ApplicationController
 
   # POST /resource/password
   def create
-    if params[:user_name]
+    if params[:user] and params[:user][:username]
+      set_flash_message(:alert, :send_instructions) if is_navigational_format?
       self.resource = resource_class.send_reset_password_instructions(params[resource_name])      
     else
+      set_flash_message(:alert, :email_instructions) if is_navigational_format?
       self.resource = resource_class.send_reset_password_instructions(params[resource_name],'username_req')
     end
-
     if successful_and_sane?(resource)
-      set_flash_message(:notice, :send_instructions) if is_navigational_format?
+      #set_flash_message(:alert, :send_instructions) if is_navigational_format?
       respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
     else
       respond_with_navigational(resource){ render_with_scope :new }
