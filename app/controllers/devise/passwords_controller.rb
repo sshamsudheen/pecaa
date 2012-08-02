9861219d29fe54b3cfb6bf9fd7319a2cb9d58df1
Manikandan K <manikandan.nitt@gmail.com>
2012-08-02 23:02:51 +0530

    Shams changes pushed to the server

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -12,16 +12,25 @@ class Devise::PasswordsController < ApplicationController
   # POST /resource/password
   def create
     if params[:user] and params[:user][:username]
-      set_flash_message(:alert, :send_instructions) if is_navigational_format?
+   #   set_flash_message(:alert, :send_instructions) if is_navigational_format?
       self.resource = resource_class.send_reset_password_instructions(params[resource_name])      
     else
-      set_flash_message(:alert, :email_instructions) if is_navigational_format?
+  #    set_flash_message(:alert, :email_instructions) if is_navigational_format?
       self.resource = resource_class.send_reset_password_instructions(params[resource_name],'username_req')
     end
     if successful_and_sane?(resource)
       #set_flash_message(:alert, :send_instructions) if is_navigational_format?
       respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
     else
+      @userexist=User.where(" username = ?" , params[resource_name][:username])		
+      if @userexist.size < 1		
+	set_flash_message(:notice, :username_not_found)
+      else
+ 	@emailexist=User.where(" email = ?" , params[resource_name][:email])
+	if @emailexist.size == 0
+          set_flash_message(:notice, :email_not_found)
+	end	
+      end		
       respond_with_navigational(resource){ render_with_scope :new }
     end
   end
